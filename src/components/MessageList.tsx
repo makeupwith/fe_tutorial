582ff86b9dd3088b5bd5300e10aed329f696e1e5
makeupwith <yudai.kato@sun-asterisk.com>
2022-05-13 08:35:10 +0900

    finish lesson 3

---
+++ src/components/MessageList.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+
+type Props = {
+    messageList: string[];
+};
+
+// TODO: message の配列を受け取り、それをJSX 内でrender する
+const MessageList: FC<Props> = (props) => {
+    return (
+        <Stack>
+            {props.messageList.map((message) => {
+                return <Box>{message}</Box>;
+            })}
+        </Stack>
+    );
+};
+
+export default MessageList;
