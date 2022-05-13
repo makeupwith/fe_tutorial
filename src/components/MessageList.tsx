3ac6295354d5e84ff06d49335d28b2da659f7ff8
makeupwith <yudai.kato@sun-asterisk.com>
2022-05-13 17:27:26 +0900

    finished the challenge

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -3,18 +3,18 @@ import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 
 type Props = {
-    messageList: string[];
+	messageList: string[];
 };
 
 // TODO: message の配列を受け取り、それをJSX 内でrender する
 const MessageList: FC<Props> = (props) => {
-    return (
-        <Stack>
-            {props.messageList.map((message) => {
-                return <Box>{message}</Box>;
-            })}
-        </Stack>
-    );
+	return (
+		<Stack>
+			{props.messageList.map((message) => {
+				return <Box>{message}</Box>;
+			})}
+		</Stack>
+	);
 };
 
 export default MessageList;
